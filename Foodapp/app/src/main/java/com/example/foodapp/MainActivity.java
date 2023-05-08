f925f9dfb27256fc4f458eeb8fe7056c093dd982
glacee158 <jin7134@naver.com>
2023-05-09 02:03:02 +0900

    modify foodapp

--- Foodapp/app/src/main/java/com/example/foodapp/MainActivity.java
+++ Foodapp/app/src/main/java/com/example/foodapp/MainActivity.java
@@ -11,11 +11,19 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int[] image_id_arr = {R.id.ramanImageView, R.id.tteokbokkiImageView, R.id.festivalNodleImageView};
     int[] textview_id_arr = {R.id.ramanTextView, R.id.tteokbokkiTextView, R.id.festivalNoodleTextView};
+    int[] countText_id_arr = {R.id.ramanCountTextView,R.id.tteokbokkiCountTextView,R.id.festivalNoodleCountTextView};
     int[] button_id_arr = {R.id.ramanCancelButton, R.id.tteokbokkiCancelButton, R.id.festivalCancelButton};
 
     ImageView[] imageviews = new ImageView[image_id_arr.length];
     TextView[] textviews = new TextView[textview_id_arr.length];
+    TextView[] count_texts = new TextView[countText_id_arr.length];
     Button[] buttons = new Button[button_id_arr.length];
+
+    Menu[] menus = {
+            new Menu("라면", "500", "맛있는 라면"),
+            new Menu("떡볶이", "1000", "맛있는 떡볶이"),
+            new Menu("잔치국수", "1500", "맛있는 국수")
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,31 +34,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void menuInit(){
-        Menu raman = new Menu("라면", 500, "맛있는 라면");
-        Menu tteokbokki = new Menu("떡볶이", 1000, "맛있는 떡볶이");
 
         for(int i = 0; i < imageviews.length; i++){
             imageviews[i] = findViewById(image_id_arr[i]);
-            imageviews[i].setOnClickListener(imageViewSelectListerner);
+            imageviews[i].setOnClickListener(imageViewSelectListener);
         }
         for(int i = 0; i < textviews.length; i++){
             textviews[i] = findViewById(textview_id_arr[i]);
+            textviews[i].setText(menus[i].getName()+ "\n" + menus[i].getDescription() + "\n가격" + menus[i].getPrice());
+            count_texts[i] = findViewById(countText_id_arr[i]);
+            count_texts[i].setText("수량: 0");
         }
         for(int i = 0; i < buttons.length; i++){
             buttons[i] = findViewById(button_id_arr[i]);
+            buttons[i].setOnClickListener(cancelButtonListener);
         }
+
     }
 
-    private View.OnClickListener imageViewSelectListerner = new View.OnClickListener() {
+    private View.OnClickListener imageViewSelectListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            switch (view.getId()){
-                case R.id.ramanImageView:
-                    textviews[0].append("\nprice 500");
-                    break;
-                case R.id.tteokbokkiImageView:
-                    textviews[1].append("\nprice 1000");
-                    break;
+            for (int i = 0; i < imageviews.length ; i++){
+                if(imageviews[i].getId() == view.getId()){
+                    menus[i].addCount();
+                    count_texts[i].setText("수량:" + Integer.toString(menus[i].getCount()));
+                }
+            }
+        }
+    };
+
+    private View.OnClickListener cancelButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            for (int i = 0; i < buttons.length ; i++){
+                if(buttons[i].getId() == view.getId()){
+                    menus[i].removeCount();
+                    count_texts[i].setText("수량: " + Integer.toString(menus[i].getCount()));
+                }
             }
         }
     };
--- Foodapp/app/src/main/java/com/example/foodapp/Menu.java
+++ Foodapp/app/src/main/java/com/example/foodapp/Menu.java
@@ -1,39 +1,32 @@
 package com.example.foodapp;
 
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Menu {
-    private int price;
+    private String price;
     private String name;
     private String description;
+    private int count;
 
-    public Menu(String name ,int price, String description){
+    public Menu(String name ,String price, String description){
         this.name = name;
         this.price = price;
         this.description = description;
+        this.count = 0;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getName() {
         return name;
     }
-
-    public int getPrice() {
+    public String getPrice() {
         return price;
     }
-
     public String getDescription() {
         return description;
     }
+    public int getCount() {return count;}
+
+    public void addCount() {this.count++;}
+    public void removeCount() {this.count = this.count <= 0 ? 0 : this.count-1;}
 }
